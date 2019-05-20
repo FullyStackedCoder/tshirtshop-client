6a857b8f8e8e0e09a3f47601d3f70e646dbb2ce7
Praveen <praveen@Praveens-iMac.local>
2019-05-20 13:05:43 +0530

    Initial full code commit

---
+++ config.js
@@ -0,0 +1,4 @@
+export const endpoint = `https://tshirtshop-apollo-prod.herokuapp.com/graphql`;
+export const endpointImages = `https://tshirtshop-apollo-prod.herokuapp.com/images/`
+export const perPage = 12;
+export const searchResultsPerQuery = 10;
