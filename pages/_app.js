fdf39bb3f3c5c57b3f8f2b137d12e274cacaa5f7
FullyStackedCoder <kvn.praveen27@gmail.com>
2019-05-28 03:11:50 +0530

    Initial full code commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import App, { Container } from "next/app";
 import { ApolloProvider } from "react-apollo";
 import withData from "../lib/withData";
 import Page from "../components/Page";
-import withApolloClient from '../lib/with-apollo-client';
 
 class MyApp extends App {
   static async getInitialProps({Component, ctx}) {
